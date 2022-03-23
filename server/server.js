ca9e7bb5645dd27b0e58ec373827179c0df5d5cd
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-24 05:09:18 +0530

    Imported packages to server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// Import packages and assign them to variables
+const express= require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app=express();
+
