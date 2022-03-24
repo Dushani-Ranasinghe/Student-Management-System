167ba64f9d203a691dee2b35c9f9f2dc2d38e926
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-24 06:30:41 +0530

    Created db connection

--- server/server.js
+++ server/server.js
@@ -12,8 +12,24 @@ const app=express();
 // else assign an availale port
 const PORT = process.env.PORT||8080;
 
+// Use declared dependencies
 app.use(cors());
 app.use(bodyParser.json);
 
-// connected mongodb database
+// get url from .env file
 const URL=process.env.MONGODB_URL;
+
+// connect to mongodb database
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopologyL:true,
+    useFindAndModify:false
+});
+
+// Created a connection and open
+const connection=mongoose.connection;
+connection.once("open",()=>{ console.log("Mongodb Connection Success!")});
+
+// once database is connected add listen connection to the port
+app.listen(PORT,()=>{console.log(`Server is up and running on port ${PORT}`)});
