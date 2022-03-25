8afdd0fd53b634575dd8f01936016c66f4744086
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-25 22:31:19 +0530

    Created a database model for students

---
+++ server/models/student.js
@@ -0,0 +1,33 @@
+//create models
+// to connect to the database use mongoose
+const mongoose = require('mongoose');
+
+// deckare schema variable to store data
+const schema = mongoose.Schema;
+
+// properties of student
+const studentSchema = new schema({
+    name: {
+        type:String,
+        required:true
+    },
+    dob:{
+        type:Date,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    indexNumber:{
+        type:Number,
+        required:true
+    }
+
+})
+
+// Document,schema
+const Student = mongoose.model("Student",studentSchema);
+
+// export model
+module.exports = Student;
